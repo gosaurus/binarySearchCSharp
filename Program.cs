474aabf27d805917ed1aac94447e444a96af98df
S Chiang <63751262+gosaurus@users.noreply.github.com>
2025-03-24 21:06:31 +0000

    Added check for case if result does not exist in list

--- Program.cs
+++ Program.cs
@@ -3,15 +3,6 @@
     class binarySearch
     {
 
-        public static int getBisectPoint(int count)
-        {
-            //if count is even,  
-            if (count % 2 == 0)
-                return count/ 2;
-            //if count is odd:
-            return (count - 1)/ 2;
-        }
-
         public static bool checkMatch(string animalAtMidIndex, string userword)
         {
             if (animalAtMidIndex == userword)
@@ -34,40 +25,26 @@
             Console.WriteLine("Beginning search...");
             bool continueSearch = true;
             while (continueSearch) 
+
             {
                 //Get bisect point. 
-                int midIndex = animals.Count % 2 == 0? animals.Count/2 : (animals.Count-1)/2;
+                int midIndex = animals.Count % 2 == 0 ? animals.Count/2 : (animals.Count-1)/2;
+                
                 Console.WriteLine($"Animal at midindex = {animals[midIndex]}, midIndex value = {midIndex}");
                 continueSearch = checkMatch(animals[midIndex], userWord);
 
-                //Dealing with endgame  // can this be simplified???
+                //If there remaining elements in list are <= 2, run these final checks
                 if (animals.Count <= 2) 
                 {
                     Console.WriteLine($"Checking at index 0 {animals[0]} and index 1 {animals[1]} against {userWord}");
                     continueSearch = checkMatch(animals[0], userWord);
                     continueSearch = checkMatch(animals[1], userWord);
+                    Console.WriteLine("No matching animal name found in list. Exiting program...");
+                    continueSearch = false;
                 }
 
-                //if false, check on either side of bisect. 
-                if (userWord[0] > animals[midIndex][0])// check if userWord char is greater than midIndex
-                {
-                    //remove first half of list
-                    Console.WriteLine($"1. Confirming Count of animals list before removing range to left of mid Index= {animals.Count}");
-                    Console.WriteLine($"2.Value of userword char greater than midIndex char: {userWord[0]} > {animals[midIndex][0]}");
-                    if (animals.Count % 2 == 0) 
-                    {
-                        Console.WriteLine($"Count is Even. Removing from 0 {animals[0]} to midIndex animal {animals[midIndex]}");
-                        animals.RemoveRange(0, midIndex + 1);
-                        Console.WriteLine($"\tRemoved animals less than last midIndex, new list count = {animals.Count}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Count is Odd. Removing from 0 {animals[0]} to midIndex animal {animals[midIndex]}");
-                        animals.RemoveRange(0, midIndex);
-                        Console.WriteLine($"\tRemoved animals less than last midIndex, new list count = {animals.Count}");
-                    }
-                }
-                else if (userWord[0] == animals[midIndex][0])
+                //Linear search check if userword and word at midindex start with same char
+                if (userWord[0] == animals[midIndex][0])
                 {
                     //loop through entries in dictionary.
                     List<string> sameLetterAnimals = [];
@@ -84,16 +61,31 @@
                             continueSearch = false;
                     }
                 }
+
+                //if false, check on either side of bisect. 
+                else if (userWord[0] > animals[midIndex][0])// check if userWord char is greater than midIndex
+                {
+                    //remove first half of list
+                    Console.WriteLine($"Value of userword char greater than midIndex char: {userWord[0]} > {animals[midIndex][0]}");
+                    if (animals.Count % 2 == 0) 
+                    {
+                        Console.WriteLine($"Count is Even. Removing from 0 {animals[0]} to midIndex animal {animals[midIndex]}");
+                        animals.RemoveRange(0, midIndex + 1);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Count is Odd. Removing from 0 {animals[0]} to midIndex animal {animals[midIndex]}");
+                        animals.RemoveRange(0, midIndex);
+                    }
+                }
                 
                 else // userWord char is smaller than animals[midIndex][0], remove range greater than midIndex
                 {
                     Console.WriteLine($"Confirming {userWord[0]} is < {animals[midIndex][0]}");
                     Console.WriteLine($"Removing animals greater and including midIndex {animals[midIndex]} to end {animals.Count-1}");
                     animals.RemoveRange(midIndex, animals.Count - midIndex);
-                    Console.WriteLine($"New Animals count = {animals.Count}");
                 }
             }
-
         }
     }
 }
